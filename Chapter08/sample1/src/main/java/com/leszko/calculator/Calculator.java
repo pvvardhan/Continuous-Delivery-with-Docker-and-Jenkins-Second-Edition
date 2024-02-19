c79747ca178929d364977814383b2c49f18b9f66
Vishnu <pv.vardhan@gmail.com>
2024-02-19 16:58:54 -0600

    fixed the javadoc comments

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * This Calculator class computes the sum of two numbers
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
